3248b7ee04fbfa88688854b47db2395a0486e06d
naman1995 <namanjain946@gmail.com>
2021-08-07 12:35:36 +0530

    created folder

--- sql/cgd.sql
+++ sql/cgd.sql
@@ -9,7 +9,7 @@ select c.concept_id from dbo.concept c,dbo.vocabulary v where standard_concept =
 #3
 select * from dbo.concept c, dbo.domain d where c.concept_code = '105694' and d.domain_id = 'Drug';
 select * from dbo.vocabulary v,dbo.concept c  where v.vocabulary_id = 'RxNorm' and c.concept_id = '44819104';
-
+ 
 #4
 select from dbo.concept_relationship cr, dbo.relationship r
 where cr.concept_id_1 = '1301125' and r.relationship_id = cr.relationship_id;
